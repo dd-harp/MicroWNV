a5910284aad1d2d6b1a5f8b4340506787cf25754
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-12-16 21:53:04 -0800

    better tests

--- tests/testthat/test-mosquito_RM.R
+++ tests/testthat/test-mosquito_RM.R
@@ -57,7 +57,7 @@ test_that("RM model setup is working", {
 })
 
 
-test_that("deterministic RM step is working with pulse of infection", {
+test_that("deterministic RM step is working with pulse of infection, no dispersal", {
   tmax <- 20
 
   a <- 0.3
@@ -115,7 +115,7 @@ test_that("deterministic RM step is working with pulse of infection", {
 })
 
 
-test_that("stochastic RM step is working with pulse of infection", {
+test_that("stochastic RM step is working with pulse of infection, no dispersal", {
   tmax <- 20
 
   a <- 0.3
@@ -135,37 +135,40 @@ test_that("stochastic RM step is working with pulse of infection", {
   mod$mosquito$kappa <- rep(1, 3)
   step_mosquitoes(model = mod)
 
-  # expect_true(all(mod$mosquito$Y > 0))
-  # expect_true(all(mod$mosquito$Z == 0))
-  # expect_true(all(mod$mosquito$ZZ[1:2, ] == 0))
-  # expect_equal(mod$mosquito$ZZ[3, ], mod$mosquito$Y)
+  expect_true(all(mod$mosquito$Y > 0))
+  expect_true(all(mod$mosquito$Z == 0))
+  expect_true(all(mod$mosquito$ZZ[1:2, ] == 0))
+  expect_equal(mod$mosquito$ZZ[3, ], mod$mosquito$Y)
 
   # time = 2
   mod$mosquito$kappa <- rep(0, 3)
   mod$global$tnow <- 2
   step_mosquitoes(model = mod)
 
-  # expect_true(all(mod$mosquito$Y > 0))
-  # expect_true(all(mod$mosquito$Z == 0))
-  # expect_equal(mod$mosquito$ZZ[2, ], mod$mosquito$Y)
-  # expect_true(all(mod$mosquito$ZZ[-2, ] == 0))
+  expect_true(all(mod$mosquito$Y > 0))
+  expect_true(all(mod$mosquito$Z == 0))
+  expect_equal(mod$mosquito$ZZ[2, ], mod$mosquito$Y)
+  expect_true(all(mod$mosquito$ZZ[-2, ] == 0))
 
   # time = 3
   mod$global$tnow <- 3
   step_mosquitoes(model = mod)
 
-  # expect_true(all(mod$mosquito$Y > 0))
-  # expect_true(all(mod$mosquito$Z == 0))
-  # expect_equal(mod$mosquito$ZZ[1, ], mod$mosquito$Y)
-  # expect_true(all(mod$mosquito$ZZ[-1, ] == 0))
+  expect_true(all(mod$mosquito$Y > 0))
+  expect_true(all(mod$mosquito$Z == 0))
+  expect_equal(mod$mosquito$ZZ[1, ], mod$mosquito$Y)
+  expect_true(all(mod$mosquito$ZZ[-1, ] == 0))
 
   # time = 4 (expect Z mosquitoes)
   mod$global$tnow <- 4
   step_mosquitoes(model = mod)
 
-  # expect_true(all(mod$mosquito$Y > 0))
-  # expect_equal(mod$mosquito$Y, mod$mosquito$Z)
-  # expect_true(all(mod$mosquito$ZZ == 0))
+  expect_true(all(mod$mosquito$Y > 0))
+  expect_equal(mod$mosquito$Y, mod$mosquito$Z)
+  expect_true(all(mod$mosquito$ZZ == 0))
+
+  expected <-  (M * a) * (0.9^4)
+  expect_true(all(abs(mod$mosquito$Z - expected) / expected < 0.025))
 
 })
 
